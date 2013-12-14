74da861ba51faf01e43da407d820f2de946a50fc
vladislav <bezverhiy@a2design.biz>
2013-12-14 15:58:22 +0700

    Add BaseAction class

---
+++ BaseAction.js
@@ -0,0 +1,16 @@
+define(function() {
+    var BaseAction = function(options) {
+
+        var self = this;
+        self.subject = options.subject;
+        self.beforeCallback = (typeof options.before == 'undefined') ? function() {} : options.before;
+        self.afterCallback = (typeof options.after == 'undefined') ? function() {} : options.after;
+        self.performAction = function(callback, Environment, options) {
+            self.beforeCallback(Environment, options);
+            callback(Environment, options);
+            self.afterCallback(Environment, options);
+
+        }
+
+    }
+})
