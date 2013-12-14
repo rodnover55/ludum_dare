0da7386a9dfafd8b9298a1d185a8b4e2d4e0fce7
vladislav <bezverhiy@a2design.biz>
2013-12-14 16:45:42 +0700

    Added Game class

--- Game.js
+++ Game.js
@@ -1,3 +1,33 @@
-define(function() {
+define(['baseMan', 'baseTool', 'ability', 'baseSubject', 'baseAction'], function(BaseMan, BaseTool, Ability, BaseSubject, BaseAction) {
 
+    var Game = function(options) {
+        var self = this;
+        var optionName;
+
+        for (optionName in options) {
+            self[optionName] = self.parseOption(options, optionName);
+        }
+
+
+
+
+    }
+
+    Game.prototype.parseOption = function(optionsList, optionName) {
+        var parseObjList = {
+            mans: BaseMan,
+            tools: BaseTool,
+            abilities: Ability,
+            subjects: BaseSubject,
+            actions: BaseAction
+        }
+        var objects = {};
+        var i;
+        for (i in optionsList[optionName]) {
+            objects[i] = new parseObjList[optionName](optionsList[optionName][i]);
+        }
+        return objects;
+    }
+
+    return Game;
 })
