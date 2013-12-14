251ea7452eecc26f053961acc704b666fe5dfc43
Vladislav Bezverkhiy <rabbiabram@gmail.com>
2013-12-14 23:13:03 +0600

    Did some refactoring

--- BaseAction.js
+++ BaseAction.js
@@ -4,16 +4,14 @@ define(function() {
         var self = this;
         self.sprite = options.sprite;
         self.name = options.name;
-
         self.beforeCallback = (typeof options.before == 'undefined') ? function() {} : options.before;
         self.afterCallback = (typeof options.after == 'undefined') ? function() {} : options.after;
 
 
-        self.performAction = function(callback, Environment, options) {
-            self.beforeCallback(Environment, options);
-            callback(Environment, options);
-            self.afterCallback(Environment, options);
-
+        self.performAction = function(ActionCaller, Subject, options) {
+            self.beforeCallback(Subject, options);
+            options.callback(ActionCaller, Subject, options);
+            self.afterCallback(Subject, options);
         }
 
     }
