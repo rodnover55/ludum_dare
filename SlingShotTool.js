5a0c6d7b5bd788410caa8b606fed306969bab7e5
vladislav <bezverhiy@a2design.biz>
2013-12-15 14:56:55 +0700

    Did some refactoring

--- SlingShotTool.js
+++ SlingShotTool.js
@@ -2,8 +2,8 @@ define(function() {
     return {
         callbacks: {
             success: function(self, caller) {
-                caller.addTool(self);
-                caller.activeAbility = 'slingshooting';
+                caller.man.addTool(self);
+                caller.man.activeAbility = self.game.abilities.slingshooting;
                 console.log(self, caller, 'success');
             },
             fail: function(self, caller) {
