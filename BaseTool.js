52657702bcb33ae9668a857811c82093bb50b62e
vladislav <bezverhiy@a2design.biz>
2013-12-15 13:43:09 +0700

    Did some changed in tools

--- BaseTool.js
+++ BaseTool.js
@@ -1,12 +1,16 @@
 define(['ability'], function(Ability){
-    var BaseTool = function(options) {
+    var BaseTool = function(options, game) {
         var self = this;
         self.abilities = self.parseAbilities(options.abilities);
         self.name = options.name;
         self.unique = (typeof options.unique == 'undefined') ? false : options.unique;
         self.canList = options.canList;
+        self.game = game;
         self.icon = options.icon;
         self.big = (typeof options.big == 'undefined') ? false : options.big;
+        self.actor = null;
+        self.callbacks = options.callbacks;
+        //TODO: захуюяр актора
 
         self.clone = function() {
             return new BaseTool(
@@ -18,6 +22,10 @@ define(['ability'], function(Ability){
                 }
             );
         }
+
+
+
+
 
 
         self.addAbilities = function(abilities) {
@@ -41,6 +49,30 @@ define(['ability'], function(Ability){
 
     }
 
+    BaseTool.prototype.doIt = function(ability) {
+        var self = this;
+        if (self.isManCanUseAbility(ability.man)) {
+            self.callbacks.success(self, ability);
+        } else {
+            self.callbacks.fail(self, ability);
+        }
+    }
+
+    BaseTool.prototype.isManCanUseAbility = function(man) {
+        var self = this;
+        man = man || this.game.currentCharacter;
+        if (this.canList.length == 0) {
+            return false;
+        }
+
+        for (var index in this.canList) {
+            if ((index in this.canList) && (this.canList[index] == man.activeAbility.name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     BaseTool.prototype.isManCanUse = function(man) {
         for (var index in this.canList) {
             if (this.canList[index].name == man.name) {
@@ -55,6 +87,10 @@ define(['ability'], function(Ability){
             list[abilities[i].name] = new Ability(abilities[i]);
         }
         return list;
+    }
+
+    BaseTool.prototype.register = function(container) {
+        this.actor.register(container);
     }
 
     return BaseTool;
