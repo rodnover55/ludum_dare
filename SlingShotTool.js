402b993b25c6580e70ef4f1ba71a43b62f321c21
vladislav <bezverhiy@a2design.biz>
2013-12-15 14:15:07 +0700

    Did some changed in tools

--- SlingShotTool.js
+++ SlingShotTool.js
@@ -2,6 +2,8 @@ define(function() {
     return {
         callbacks: {
             success: function(self, caller) {
+                caller.addTool(self);
+                caller.activeAbility = 'slingshooting';
                 console.log(self, caller, 'success');
             },
             fail: function(self, caller) {
